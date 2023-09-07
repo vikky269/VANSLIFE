8231e00458b302c8d63c7da4932891a0299f7fa0
Amune victor <viktorefi2001@gmail.com>
2023-09-07 05:44:55 -0700

    God Abeg

--- index.jsx
+++ index.jsx
@@ -3,6 +3,9 @@ import ReactDOM from 'react-dom/client';
 import Home from './pages/Home';
 import About from './pages/About';
 import { BrowserRouter, Routes, Route, Link } from 'react-router-dom';
+import './server'
+import Vans from './pages/Vans/Vans';
+import VanDetail from './pages/Vans/VanDetail';
 
 function App() {
 
@@ -12,11 +15,14 @@ function App() {
       <Link to="/" className='site-logo'>#VANLIFE</Link>
       <nav>
         <Link to="/About">About</Link>
+        <Link to="/Vans">Vans</Link>
       </nav>
      </header>
     <Routes>
       <Route  path='/' element={<Home/>}/>
       <Route  path='/About' element={<About/>}/>
+      <Route  path='/Vans' element={<Vans/>}/>
+      <Route path='/Vans/:id' element= {<VanDetail/>}/>
     </Routes>
     </BrowserRouter>
   )
